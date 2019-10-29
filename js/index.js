5c11d182e455f34bcc60b8e8b3f26889c53f1b81
Joseph Graham <Joseph@Chasingthefiks.net>
2019-10-29 13:47:20 -0600

    1. Nav on Click, 2.   Logo on Mouse Enter / Mouse Leave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+const lgOnMouse = document.querySelector(".logo-heading");
+lgOnMouse.addEventListener("mouseenter", () => {
+    lgOnMouse.style.color = "red";
+})
+lgOnMouse.addEventListener("mouseleave", () => {
+    lgOnMouse.style.color = "black";
+})
+
+const nvOnClick = document.querySelectorAll("nav a");
+nvOnClick.forEach((nv) => {
+    nv.addEventListener("click", () => {
+        alert("You Clicked Me!");
+
+    })
+})
